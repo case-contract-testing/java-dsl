2b40dafbef07a767054c7fcdc9e4819c217b9ccf
Timothy Jones <timothy.l.jones@gmail.com>
2024-02-11 23:57:53 +1100

    chore: Parameterise boundary mapper

--- src/main/java/io/contract_testing/contractcase/BoundaryDefinitionMapper.java
+++ src/main/java/io/contract_testing/contractcase/BoundaryDefinitionMapper.java
@@ -1,10 +1,11 @@
 package io.contract_testing.contractcase;
 
 import io.contract_testing.contractcase.case_boundary.BoundaryMockDefinition;
+import io.contract_testing.contractcase.case_example_mock_types.mocks.base.AnyMockDescriptor;
 
 class BoundaryDefinitionMapper {
 
-  static BoundaryMockDefinition map(ExampleDefinition definition) {
+  static <T extends AnyMockDescriptor> BoundaryMockDefinition  map(ExampleDefinition<T> definition) {
     return new BoundaryMockDefinition.Builder().definition(definition.getDefinition())
         .states(definition.getStates()).build();
   }
